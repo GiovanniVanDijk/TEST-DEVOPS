e3c21ea14a368ebc7a2b8af48fbc62d9823b8bba
GiovanniVanDijk <152384349+GiovanniVanDijk@users.noreply.github.com>
2025-01-09 20:30:05 +0100

    Create program.cs

---
+++ Frontend/program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EasyDevOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ASCII Logo
+            Console.WriteLine("   _____ _______ __  __ ");
+            Console.WriteLine("  |_   _|__   __|  \\/  |");
+            Console.WriteLine("    | |    | |  | \\  / |");
+            Console.WriteLine("    | |    | |  | |\\/| |");
+            Console.WriteLine("   _| |_   | |  | |  | |");
+            Console.WriteLine("  |_____|  |_|  |_|  |_|");
+            Console.WriteLine();
+            Console.WriteLine("   International Transport Management");
+            Console.WriteLine("            EASY DEVOPS");
+            Console.WriteLine();
+
+            // Welkomsboodschap
+            Console.WriteLine("Welcome to the EasyDevOps App!");
+            Console.WriteLine("Powered by ITM.");
+        }
+    }
+}
